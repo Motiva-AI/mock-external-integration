6319bae035399eaabccfd889afcfadefedc5623f
Paul Lam <paul@quantisan.com>
2017-08-28 08:21:23 +0900

    add servlet dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "2.0.0-alpha7"]
+
+                 ;; server
                  [http-kit "2.2.0"]
+                 [javax.servlet/servlet-api "2.5"]
 
                  [environ "1.1.0"]
                  [prismatic/schema "1.1.6"]]
